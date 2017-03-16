fc50df3245cef839950bc03c3429f1775980caeb
ragnraok <okone1288@gmail.com>
2017-03-16 17:11:17 +0800

    update android example

--- example/handwritten-src/java/com/dropbox/textsort/SuperTextboxListenerImpl.java
+++ example/handwritten-src/java/com/dropbox/textsort/SuperTextboxListenerImpl.java
@@ -8,7 +8,7 @@ import static com.dropbox.textsort.Util.TAG;
  * Created by ragnarok on 2017/3/16.
  */
 
-public class SuperTextboxListenerImpl implements SuperListtener {
+public class SuperTextboxListenerImpl extends SuperSuperListenerImpl implements SuperListtener {
     @Override
     public void update(ItemList items) {
         Log.i(TAG, "update " + items.getItems().toString());
